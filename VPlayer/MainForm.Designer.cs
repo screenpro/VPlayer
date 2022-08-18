d0ea14c7db3dbc48bbe09d9e11a1cab1c8d3677c
Dennis Walter <denniswalter@runbox.com>
2022-08-18 08:38:16 +0200

    Initial Commit

---
+++ VPlayer/MainForm.Designer.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace VPlayer
+{
+    partial class MainForm
+    {
+        /// Required designer variable.
+        private IContainer components = null;
+
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "MainForm";
+        }
+
+        #endregion
+    }
+}
---
+++ VPlayer/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VPlayer
+{
+    public partial class MainForm : Form
+    {
+        public Button button1;
+        public Button button2;
+        public MainForm()
+        {
+            InitializeComponent();
+            button1 = new Button();
+            button1.Size = new Size(80, 40);
+            button1.Location = new Point(20, 20);
+            button1.Text = "Button1";
+            this.Controls.Add(button1);
+            button1.Click += new EventHandler(button1_Click);
+
+            button2 = new Button();
+            button2.Size = new Size(80, 40);
+            button2.Location = new Point(120, 20);
+            button2.Text = "Button2";
+            this.Controls.Add(button2);
+            button2.Click += new EventHandler(button2_Click);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 1 Clicked!");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 2 clicked!");
+        }
+    }
+}
